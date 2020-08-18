d59c4064c8ea35393d562d581f31773cd67d6578
mozg3000 <i.luka@outlook.com>
2020-08-18 14:34:32 +0300

    [feature] personal page has got tabs now

--- js/main.js
+++ js/main.js
@@ -60,5 +60,26 @@ $(document).ready(function() {
     } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
         $("#mobile-nav, #mobile-nav-toggle").hide();
     }
+	
+	$('.tablinks').on('click', openTab);
 });
+function openTab(evt) {
+	// Declare all variables
+	let i, tabcontent, tablinks, tabName;
+	tabName = evt.target.dataset.id;
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "flex";
+	evt.currentTarget.className += " active";
+}
